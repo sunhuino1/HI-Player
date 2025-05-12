2d6eb11fb0ccc6b91c7ac80590c5b55d8ec00362
sunhui <775033@qq.com>
2025-05-12 21:53:21 +0800

    feat: add install.sh

---
+++ audioNetworkBridge/aspberryPi-4B-Debian/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo chmod a+x ./startANB.sh
+sudo chmod a+x ./stopANB.sh
+sudo chmod a+x ./audioNetworkBridge
+
+sed -e "s#/home/HI-Player#$(pwd)#g" ./HI-Player.service.txt > ./HI-Player.service
+
+sudo cp ./HI-Player.service  /lib/systemd/system/HI-Player.service
+
+sudo chmod a+x /lib/systemd/system/HI-Player.service
+sudo systemctl daemon-reload
+sudo systemctl enable HI-Player.service
+
+
+
+
+
+
